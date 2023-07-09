8a1435679f8d476feedc7222999b2d024f178069
frans <31902695+pyrbin@users.noreply.github.com>
2023-07-09 15:58:44 +0200

    Update EnemyDeath.cs

--- Assets/Scripts/Audio/EnemyDeath.cs
+++ Assets/Scripts/Audio/EnemyDeath.cs
@@ -8,18 +8,6 @@ public class EnemyDeath : MonoBehaviour
     public FMODUnity.EventReference DeathEvent;
     FMOD.Studio.EventInstance death;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        PlayDeathSound();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     public void PlayDeathSound()
     {
         death = FMODUnity.RuntimeManager.CreateInstance(DeathEvent);
