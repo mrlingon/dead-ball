cb9e9eb49c36a3a8df727feda45a39b3afdd619f
Mrlingon <gustav.alvinsson1@gmail.com>
2023-07-09 13:51:18 +0200

    added game over scene and howto (not done)

---
+++ Assets/GameOverHandler.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+public class gameOverHandler : MonoBehaviour
+{
+
+    
+    private Button _exitButton;
+    private Button _restartButton;
+    private Label _score;
+
+    private VisualElement _mainContainer;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void OnEnable(){
+
+        var uiDoc = GetComponent<UIDocument>();
+        
+
+        _restartButton = uiDoc.rootVisualElement.Q("RestartButton") as Button;
+        _exitButton = uiDoc.rootVisualElement.Q("ExitButton") as Button;
+
+        _score = uiDoc.rootVisualElement.Q("Score") as Label;
+
+        
+
+        _restartButton.RegisterCallback<ClickEvent>(evt => RestartGame());
+        _exitButton.RegisterCallback<ClickEvent>(evt => QuitGame());
+
+    }
+
+    private void PrintClickMessage() {
+
+        Debug.Log($"{"button"} was clicked!");
+        SceneManager.LoadScene(1);
+      }
+      private void RestartGame() {
+
+        Debug.Log($"{"button"} was clicked!");
+        SceneManager.LoadScene(1);
+      }
+
+
+    
+
+    
+    private void QuitGame(){
+            Debug.Log("Quit game!");
+    }
+
+}
