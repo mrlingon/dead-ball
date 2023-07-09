1f53d0914a9f46b967badbaa647b890bedb7d555
smultronbusken <fredholmisak@outlook.com>
2023-07-09 14:24:11 +0200

    level manager change, fixed shirt sprite bug

--- Assets/Scripts/Ball/BallPhysicsBody.cs
+++ Assets/Scripts/Ball/BallPhysicsBody.cs
@@ -77,8 +77,8 @@ public class BallPhysicsBody : MonoBehaviour
 
     public bool Frozen { get; private set; } = false;
 
-    public int grabCooldown = 1000; // Frans när du ser detta säg till mig
-    public bool canBeGrabbed = true; // Frans när du ser detta säg till mig
+    public int grabCooldown = 1000;
+    public bool canBeGrabbed = true;
 
     public bool IsGrounded()
     {
